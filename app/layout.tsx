f8faec2fa15f88eab3fd8e4d9256fcb5037a6c25
jonathan Service <jonathanservice@jonathans-MacBook-Air.local>
2025-10-05 12:34:38 +0100

    Gtag update

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,24 @@ export default function RootLayout({
             gtag('config', 'G-JG85N89P8Q');
           `}
         </Script>
+         <Script id="google-tag-manager">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WFJW4MN');
+          `}
+        </Script>
       <body className={`${inter.className} bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200`}>
+     <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WFJW4MN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
      {children}
       </body>
          <Script 
